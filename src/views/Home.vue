07e86ab521ddca9c374c460ed529a5ace07dcf03
moroball14 <daiki.mr.0330@gmail.com>
2020-05-10 13:49:54 +0900

    process.envで環境変数を取得できることを確認

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="greet">Greet</button>
   </div>
 </template>
 
@@ -13,6 +14,17 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    greet: function (event) {
+      // メソッド内の `this` は、 Vue インスタンスを参照します
+      alert('Hello ' + this.name + '!')
+      // `event` は、ネイティブ DOM イベントです
+      if (event) {
+        alert(event.target.tagName)
+        // console.log(process.env); procecc.envで環境変数取得できることを確認
+      }
+    }
   }
 }
 </script>
